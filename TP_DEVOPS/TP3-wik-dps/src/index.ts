ddb74671929f4e58e3c37774211a92e4b7debffc
DorianAlbert <dorianalbert34510@gmail.com>
2023-10-17 11:41:11 +0200

    update

--- TP_DEVOPS/TP3-wik-dps/src/index.ts
+++ TP_DEVOPS/TP3-wik-dps/src/index.ts
@@ -1,23 +1,30 @@
-import { createServer, IncomingMessage, ServerResponse } from "http"
+import { createServer, IncomingMessage, ServerResponse } from "http";
+import * as os from "os";
 
 // Method used to handle incoming requests
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
   try {
     // Only send JSON if HTTP verb is GET and url is /ping
     if (req.method === "GET" && req.url === "/ping") {
-      res.setHeader("Content-Type", "application/json")
-      res.write(JSON.stringify(req.headers))
+      res.setHeader("Content-Type", "application/json");
+      const responseJson = {
+        headers: req.headers,
+        hostname: os.hostname(),
+      };
+      res.write(JSON.stringify(responseJson));
       res.end();
+      // Log the hostname
+      console.log(`Request from hostname: ${os.hostname()}`);
     // Else return HTTP 404
     } else {
-      res.statusCode = 404
-      res.end()
+      res.statusCode = 404;
+      res.end();
     }
   // If something went wrong return HTTP 500
   } catch (err) {
-    console.error(err)
-    res.statusCode = 500
-    res.end()
+    console.error(err);
+    res.statusCode = 500;
+    res.end();
   }
 }
 
@@ -25,16 +32,16 @@ try {
   // Server creation
   const server = createServer(requestListener);
   server.listen(process.env.PING_LISTEN_PORT ?? 8080);
-  const serverAddressInfo = server.address()
+  const serverAddressInfo = server.address();
   if (!serverAddressInfo) {
-    throw new Error("No server address info")
+    throw new Error("No server address info");
   }
   if (typeof serverAddressInfo === 'string') {
-    console.log(`Server listening : ${serverAddressInfo}`)
+    console.log(`Server listening: ${serverAddressInfo}`);
   } else {
-    console.log(`Server listening : ${serverAddressInfo.address}:${serverAddressInfo.port}`)
+    console.log(`Server listening: ${serverAddressInfo.address}:${serverAddressInfo.port}`);
   }
 } catch (err) {
-  console.error(err)
-  process.exit(1)
+  console.error(err);
+  process.exit(1);
 }
